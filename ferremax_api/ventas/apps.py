377dc4b03942c85d487a434c84e4d71de22bc0f7
Ommejia <omarmejia11.a@gmail.com>
2024-05-26 23:50:32 -0400

    requeriments.md

--- ferremax_api/ventas/apps.py
+++ ferremax_api/ventas/apps.py
@@ -1,6 +1,8 @@
-from django.apps import AppConfig
+from django.urls import path
+from .api import ProductListAPI, CartItemListCreateAPI, CheckoutAPI
 
-
-class VentasConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ventas'
+urlpatterns = [
+    path('products/', ProductListAPI.as_view(), name='product_list_api'),
+    path('cart/', CartItemListCreateAPI.as_view(), name='cart_item_list_create_api'),
+    path('checkout/', CheckoutAPI.as_view(), name='checkout_api'),
+]
